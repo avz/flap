e068670d13b34b50d269027c7f3c7db1c22c5441
avz <arepo@nologin.ru>
2013-08-14 13:30:10 +0400

    Добавил клиентский режим и опции ком. строки для него

--- src/main.c
+++ src/main.c
@@ -209,12 +209,15 @@ struct Server {
 void *Server__connectionPollerThread(void *serverPtr) {
 	struct Server *server = (struct Server *)serverPtr;
 	int s;
+	int bufSize = 1024 * 1024;
 
 	while((s = accept(server->sock, NULL, NULL)) >= 0) {
+		if(setsockopt(s, SOL_SOCKET, SO_SNDBUF, &bufSize, sizeof(bufSize)) != 0)
+			perror("setsockopt(s, SOL_SOCKET, SO_SNDBUF)");
+
 		ClientsPool__lock(&server->pool);
 		ClientsPool_attachClient(&server->pool, s, O_RDONLY, 0);
 		ClientsPool__unlock(&server->pool);
-		fprintf(stderr, "New connection: %d\n", s);
 	}
 
 	return NULL;
@@ -230,7 +233,7 @@ void Server_bindUnix(struct Server *server, const char *path, char forceReuse) {
 
 	if(strlen(path) >= sizeof(addr.sun_path)) {
 		errno = ENAMETOOLONG;
-		perror("Path to Unix socket is too long");
+		perror(path);
 		exit(errno);
 	}
 
@@ -279,15 +282,80 @@ void Server_destroy(struct Server *server) {
 	ClientsPool_destroy(&server->pool);
 }
 
-int main(int argc, char *argv[]) {
+void usageAndExit(const char *cmd) {
+	fprintf(stderr,
+		"Usage: %s (-s|-c) /path/to/socket.sock\n"
+		"	-s: server mode\n"
+		"	-c: client mode (read-only)\n"
+		, cmd
+	);
+
+	exit(255);
+}
+
+int runClientMode(const char *sockPath) {
 	char buf[16 * 1024];
+	struct sockaddr_un addr;
+	int sock;
+	int sockBufSize = 1024 * 1024;
+
+	if(strlen(sockPath) >= sizeof(addr.sun_path)) {
+		errno = ENAMETOOLONG;
+		perror(sockPath);
+		exit(errno);
+	}
+
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(sock == -1) {
+		perror("socket(AF_UNIX, SOCK_STREAM, 0)");
+		exit(errno);
+	}
+
+	strncpy(addr.sun_path, sockPath, sizeof(addr.sun_path));
+	addr.sun_family = AF_UNIX;
+
+	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+		perror(sockPath);
+		return errno;
+	}
+
+	if(setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sockBufSize, sizeof(sockBufSize)) != 0)
+		perror("setsockopt(sock, SOL_SOCKET, SO_SNDBUF)");
+
+	while(1) {
+		ssize_t written = 0;
+		ssize_t r = read(sock, buf, sizeof(buf));
+		if(r <= 0) {
+			if(r < 0 && errno == EINTR)
+				continue;
+
+			return r ? errno : 0;
+		}
+
+		while(written < r) {
+			ssize_t w = write(STDOUT_FILENO, buf + written, (size_t)(r - written));
+			if(w <= 0) {
+				perror("read(STDIN_FILENO)");
+				return errno;
+			} else {
+				written += w;
+			}
+		}
+	}
+
+	/* never reached */
+	return 0;
+}
+
+int runServerMode(const char *sockPath) {
+	char buf[32 * 1024];
 	struct Server server;
 	char eof = 0;
 
 	signal(SIGPIPE, SIG_IGN);
 
 	Server_init(&server);
-	Server_bindUnix(&server, argv[1], 1);
+	Server_bindUnix(&server, sockPath, 1);
 
 	while(!eof) {
 		ssize_t r = read(STDIN_FILENO, buf, sizeof(buf));
@@ -312,7 +380,7 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 
-				perror("read(STDIN_FILENO)");
+				perror("write(STDOUT_FILENO)");
 				return errno;
 			} else {
 				written += w;
@@ -323,6 +391,39 @@ int main(int argc, char *argv[]) {
 	}
 
 	Server_destroy(&server);
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	int opt;
+	char serverMode = 0;
+	char clientMode = 0;
+
+	while((opt = getopt(argc, argv, "sc")) != -1) {
+		switch(opt) {
+			case 's':
+				serverMode = 1;
+			break;
+			case 'c':
+				clientMode = 1;
+			break;
+			default:
+				usageAndExit(argv[0]);
+		}
+	}
+
+	if(optind > argc)
+		usageAndExit(argv[0]);
+
+	if((serverMode && clientMode) || (!serverMode && !clientMode))
+		usageAndExit(argv[0]);
+
+	if(serverMode) {
+		runServerMode(argv[optind]);
+	} else if(clientMode) {
+		runClientMode(argv[optind]);
+	}
 
 	return EXIT_SUCCESS;
 }
